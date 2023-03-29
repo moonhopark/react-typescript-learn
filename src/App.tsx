57cbdfb9936ffa47bfbd2acb818c0680f0ae0bf6
moonhopark <moonho4199@naver.com>
2023-03-29 11:45:25 +0900

    feat: event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,16 @@
 import './App.css';
-import Greet from './components/typescript-basic/Greet';
-import Heading from './components/typescript-basic/Heading';
-import Oscar from './components/typescript-basic/Oscar';
-import Person from './components/typescript-basic/Person';
-import PersonList from './components/typescript-basic/PersonList';
-import Status from './components/typescript-basic/Status';
+import Button from './components/typescript-basic/Button';
+import Input from './components/typescript-basic/Input';
 
 function App() {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne',
-  };
-
-  const nameList = [
-    {
-      first: 'Bruce',
-      last: 'Wayne',
-    },
-    {
-      first: 'Clark',
-      last: 'Kent',
-    },
-    {
-      first: 'Princess',
-      last: 'Diana',
-    },
-  ];
-
   return (
     <div className="App">
-      <Greet name="moonho" isLoggedIn={true} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
-      <Status status="loading" />
-      <Heading>Placeholder text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicpario!</Heading>
-      </Oscar>
+      <Button
+        handleClick={(event, id) => {
+          console.log('Button clicked', event);
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
---
+++ src/components/typescript-basic/Input.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  value: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Input = (props: Props) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+  };
+
+  return <input type="text" value={props.value} onChange={handleInputChange} />;
+};
+
+export default Input;
