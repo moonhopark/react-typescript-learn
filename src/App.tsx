41ab60b8f1d018b484ee42bf7a2520c5add35ef1
moonhopark <moonho4199@naver.com>
2023-03-29 15:37:50 +0900

    feat: useState type

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css';
 import Button from './components/typescript-basic/Button';
 import Container from './components/typescript-basic/Container';
 import Input from './components/typescript-basic/Input';
+import User from './components/typescript-basic/state/User';
 
 function App() {
   return (
     <div className="App">
       <Container styles={{ border: '1px solid black', padding: '1rem' }} />
+      <User />
     </div>
   );
 }
---
+++ src/components/typescript-basic/state/User.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+type AuthUser = {
+  name: string;
+  email: string;
+};
+
+const User = () => {
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
+
+  const handleLogin = () => {
+    setUser({
+      name: 'moonho',
+      email: 'moonho@example.com',
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+
+      <div>User name is {user.name}</div>
+      <div>User email is {user.email}</div>
+    </div>
+  );
+};
+
+export default User;
