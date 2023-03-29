ad21af023d796a56edea7555cd3266513f575823
moonhopark <moonho4199@naver.com>
2023-03-29 16:01:16 +0900

    feat: useReducer type

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css';
-import Button from './components/typescript-basic/Button';
-import Container from './components/typescript-basic/Container';
-import Input from './components/typescript-basic/Input';
-import User from './components/typescript-basic/state/User';
+import { Counter } from './components/typescript-basic/state/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Container styles={{ border: '1px solid black', padding: '1rem' }} />
-      <User />
+      <Counter />
     </div>
   );
 }
